124656fa6fc244ce395dec4d6001450684071d31
Akinlua Bolamigbe Jomi <bolamigbeakinlua@gmail.com>
2020-08-04 00:56:55 +0100

    added mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alob-mtc/microservices-in-go
+
+go 1.14
--- main.go
+++ main.go
@@ -11,8 +11,11 @@ func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello world")
-		d, _ := ioutil.ReadAll(r.Body)
-
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
 		fmt.Fprintf(rw, "Hello %s\n", d)
 	})
 
